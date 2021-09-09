532f1461149f969d54d0d147e467fdf438114f92
jamil <jamilujibrilla01@gmail.com>
2021-09-09 09:22:53 +0100

    style update

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 let menuCont = document.querySelector(".menu-container");
+let addToCart = document.querySelectorAll(".cart");
+let notif = document.querySelector(".cart-notifs");
+
+addToCart.forEach((item) => {
+  item.addEventListener(
+    "click",
+    function () {
+      item.innerHTML = `<i class="fas fa-check"></i>`;
+      setTimeout(() => {
+        notif.style.display = "flex";
+      }, 50);
+      setTimeout(() => {
+        notif.style.display = "none";
+      }, 2000);
+    },
+    { once: true }
+  );
+});
+
+let allList = document.getElementsByTagName("a");
